66134b28fa8d2b62b0aa399fcdabe2798941127c
muyi-jian <2628168756@qq.com>
2024-08-04 20:35:48 +0800

    Initial commit

---
+++ yjablog-auth/src/main/java/com/yjablog/auth/YjablogAuthApplication.java
@@ -0,0 +1,13 @@
+package com.yjablog.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjablogAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjablogAuthApplication.class, args);
+    }
+
+}
